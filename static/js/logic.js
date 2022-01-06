dd39457b1adc917dcbcf10209fb7a693acc4788a
nladkins <adkins.norman@epa.gov>
2022-01-05 22:09:44 -0500

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -4,6 +4,7 @@ var myMap = L.map("map", {
     0, 0
   ],
   zoom: 2.4,
+  layer: [street, topo]
 });
 
 
@@ -21,9 +22,9 @@ var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
 
 
-function chooseColor(mag) {
-  if (mag <= 5) return "light yellow";
-  else if (mag <= 6.5) return "orange";
+function chooseColor(depth) {
+  if (depth <= 10) return "light yellow";
+  else if (depth <= 20) return "orange";
   else return"red";
 }
 
@@ -32,48 +33,30 @@ d3.json(queryUrl).then(function (data) {
   // Once we get a response, send the data.features object to the createFeatures function.
   L.geoJSON(data, {
     
-    style: function(feature) {
+    style: function(feature, latlng) {
       // Add circles to the map.
-      L.circle([data.features.geometry.coordinates[0], data.features.geometry.coordinates[1]], {
+      L.circleMarker([feature.geometry.coordinates[0], feature.geometry.coordinates[1]], {
         fillOpacity: 0.75,
         color: "white",
-        fillColor: chooseColor(feature.properties.mag),
+        fillColor: chooseColor(feature.geometry.coordinates[2]),
+        weight: feature.geometry.coordinates[2],
         // Adjust the radius.
-        radius: Math.sqrt(data.features.geometry.coordinates[2] * 1000)
-      }).bindPopup(`<h3>${data.features.properties.place}</h3><hr>
-      <h3>${new Date(data.features.properties.time)}</h3>
-      <h3>Magnitude: ${data.features.properties.mag}</h3>
-      <p>More Info <br>${data.features.properties.url}</p>`
-      )
+        radius: feature.properties.mag * 100
+      })
     },
 
+    pointToLayer: function (feature, latlng) {
+      return L.circleMarker(latlng);
+    },
+    
     onEachFeature: function(feature, layer) {
-      // Set the mouse events to change the map styling.
-      // layer.on({
-      //   // When a user's mouse cursor touches a map feature, the mouseover event calls this function, which makes that feature's opacity change to 90% so that it stands out.
-      //   mouseover: function(event) {
-      //     layer = event.target;
-      //     layer.setStyle({
-      //       fillOpacity: 0.9
-      //     });
-      //   },
-      //   // When the cursor no longer hovers over a map feature (that is, when the mouseout event occurs), the feature's opacity reverts back to 50%.
-      //   mouseout: function(event) {
-      //     layer = event.target;
-      //     layer.setStyle({
-      //       fillOpacity: 0.5
-      //     });
-      //   },
-      //   // When a feature (neighborhood) is clicked, it enlarges to fit the screen.
-      //   // click: function(event) {
-      //   //   myMap.fitBounds(event.target.getBounds());
-      //   // }
-      // });
+      
       layer.bindPopup(`<h3>${feature.properties.place}</h3><hr>
       <h3>${new Date(feature.properties.time)}</h3>
       <h3>Magnitude: ${feature.properties.mag}</h3>
       <p>More Info <br>${feature.properties.url}</p>`)
     }
 
-  }).addTo(myMap);;
+  }).addTo(myMap);
+
 });
